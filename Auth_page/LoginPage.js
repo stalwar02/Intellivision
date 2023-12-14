cce795a3f293c84a43dc84a2865468d801615929
Infinite-Null <ankit.kum.sha9933@gmail.com>
2023-12-14 23:45:18 +0530

    Done some minor changes to login and signup page

--- Auth_page/LoginPage.js
+++ Auth_page/LoginPage.js
@@ -1,50 +1,206 @@
-import { View, Text, ImageBackground, StatusBar, Image, TouchableOpacity, Dimensions } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  View,
+  Text,
+  ImageBackground,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 import React from 'react';
 import * as Animatable from 'react-native-animatable';
-import { TextInput } from 'react-native-gesture-handler';
+import {TextInput} from 'react-native-gesture-handler';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-export default function Login({ navigation }) {
+export default function Login({navigation}) {
   return (
-    <ImageBackground style={{ flex: 1 }} source={require("../Assets/pxfuel2.jpg")}>
-      <Animatable.View duration={500} useNativeDriver={true} animation="fadeInRight" style={{ flex: 1, marginTop: windowHeight * 0.15, marginLeft: windowWidth * 0.05 }}>
+    <ImageBackground
+      style={{flex: 1}}
+      source={require('../Assets/pxfuel2.jpg')}>
+      <Animatable.View
+        duration={500}
+        useNativeDriver={true}
+        animation="fadeInRight"
+        style={{
+          flex: 1,
+          marginTop: windowHeight * 0.15,
+          marginLeft: windowWidth * 0.05,
+        }}>
         <StatusBar backgroundColor={'black'} />
-        <Text style={{ fontSize: windowWidth * 0.09, fontWeight: 'bold', color: 'white' }}>Welcome</Text>
-        <Text style={{ fontSize: windowWidth * 0.09, fontWeight: 'bold', color: 'white' }}>Back!</Text>
-        <Text style={{ marginTop: windowHeight * 0.01, color: 'grey' }}>Enter your email address and password to get</Text>
-        <Text style={{ color: 'grey' }}>access your account</Text>
-        <View style={{ height: windowHeight * 0.19, width: '95%', marginTop: windowHeight * 0.05 }}>
-          <View style={{ flex: 1, width: '100%', backgroundColor: 'grey', marginBottom: windowHeight * 0.015, borderRadius: windowWidth * 0.04, opacity: 0.7, flexDirection: 'row', alignItems: 'center', paddingLeft: windowWidth * 0.04 }}>
-            <Image source={require("../Assets/at.png")} style={{ height: windowWidth * 0.05, width: windowWidth * 0.05 }} />
-            <TextInput style={{ flex: 1, marginLeft: windowWidth * 0.025 }} placeholder='Email' placeholderTextColor={'white'}></TextInput>
+        <Text
+          style={{
+            fontSize: windowWidth * 0.09,
+            fontWeight: 'bold',
+            color: 'white',
+          }}>
+          Welcome
+        </Text>
+        <Text
+          style={{
+            fontSize: windowWidth * 0.09,
+            fontWeight: 'bold',
+            color: 'white',
+          }}>
+          Back!
+        </Text>
+        <Text style={{marginTop: windowHeight * 0.01, color: 'grey'}}>
+          Enter your email address and password to get
+        </Text>
+        <Text style={{color: 'grey'}}>access your account</Text>
+        <View
+          style={{
+            height: windowHeight * 0.19,
+            width: '95%',
+            marginTop: windowHeight * 0.05,
+          }}>
+          <View
+            style={{
+              flex: 1,
+              width: '100%',
+              backgroundColor: 'grey',
+              marginBottom: windowHeight * 0.015,
+              borderRadius: windowWidth * 0.04,
+              opacity: 0.7,
+              flexDirection: 'row',
+              alignItems: 'center',
+              paddingLeft: windowWidth * 0.04,
+            }}>
+            <Image
+              source={require('../Assets/at.png')}
+              style={{height: windowWidth * 0.05, width: windowWidth * 0.05}}
+            />
+            <TextInput
+              style={{flex: 1, marginLeft: windowWidth * 0.025}}
+              placeholder="Email"
+              placeholderTextColor={'white'}
+            />
           </View>
-          <View style={{ flex: 1, width: '100%', backgroundColor: 'grey', marginBottom: windowHeight * 0.015, borderRadius: windowWidth * 0.04, opacity: 0.7, flexDirection: 'row', alignItems: 'center', paddingLeft: windowWidth * 0.04 }}>
-            <Image source={require("../Assets/key.png")} style={{ height: windowWidth * 0.05, width: windowWidth * 0.05 }} />
-            <TextInput style={{ flex: 1, marginLeft: windowWidth * 0.025 }} placeholder='Password' placeholderTextColor={'white'}></TextInput>
+          <View
+            style={{
+              flex: 1,
+              width: '100%',
+              backgroundColor: 'grey',
+              marginBottom: windowHeight * 0.015,
+              borderRadius: windowWidth * 0.04,
+              opacity: 0.7,
+              flexDirection: 'row',
+              alignItems: 'center',
+              paddingLeft: windowWidth * 0.04,
+            }}>
+            <Image
+              source={require('../Assets/key.png')}
+              style={{height: windowWidth * 0.05, width: windowWidth * 0.05}}
+            />
+            <TextInput
+              style={{flex: 1, marginLeft: windowWidth * 0.025}}
+              placeholder="Password"
+              placeholderTextColor={'white'}
+            />
           </View>
         </View>
-        <View style={{ marginTop: windowHeight * 0.05, height: windowHeight * 0.06, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '95%' }}>
-          <Text style={{ fontSize: windowWidth * 0.04, fontWeight: 'bold', color:'white' }}>Forgot?</Text>
-          <View style={{ height: '100%', width: windowWidth * 0.3, borderRadius: windowWidth * 0.03, backgroundColor: '#d1b3ff', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-            <View style={{ height: '100%', width: '70%', backgroundColor: 'rgb(102, 0, 150)', borderRadius: windowWidth * 0.03, alignItems: 'center', justifyContent: 'center' }}>
-              <Text style={{ fontSize: windowWidth * 0.06, fontWeight: 'bold' }}>Login</Text>
-            </View>
+        <View
+          style={{
+            marginTop: windowHeight * 0.05,
+            height: windowHeight * 0.06,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '95%',
+          }}>
+          <Text
+            style={{
+              fontSize: windowWidth * 0.035,
+              fontWeight: 400,
+              color: '#22A7F0',
+            }}>
+            Forgot Password ?
+          </Text>
+          <View
+            style={{
+              height: '100%',
+              width: windowWidth * 0.3,
+              borderRadius: windowWidth * 0.025,
+              backgroundColor: 'rgb(104, 89, 235)',
+              elevation: 10,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{fontSize: windowWidth * 0.035, color: 'white'}}>
+              Login
+            </Text>
+            {/* <View
+              style={{
+                height: '100%',
+                width: '70%',
+                backgroundColor: 'rgb(102, 0, 150)',
+                borderRadius: windowWidth * 0.03,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+             
+            </View> */}
           </View>
         </View>
-        <View style={{ height: windowHeight * 0.08, width: '95%', flexDirection: 'row', marginTop: windowHeight * 0.06, justifyContent: 'space-between' }}>
-          <View style={{ width: '40%', height: '100%', borderWidth: windowWidth * 0.01, borderColor: 'white', borderRadius: windowWidth * 0.05, alignItems: 'center', justifyContent: 'center' }}>
-            <Image source={require("../Assets/facebook.png")} style={{ height: windowWidth * 0.1, width: windowWidth * 0.1 }} />
+        {/* <View
+          style={{
+            height: windowHeight * 0.08,
+            width: '95%',
+            flexDirection: 'row',
+            marginTop: windowHeight * 0.06,
+            justifyContent: 'space-between',
+          }}>
+          <View
+            style={{
+              width: '40%',
+              height: '100%',
+              borderWidth: windowWidth * 0.01,
+              borderColor: 'white',
+              borderRadius: windowWidth * 0.05,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image
+              source={require('../Assets/facebook.png')}
+              style={{height: windowWidth * 0.1, width: windowWidth * 0.1}}
+            />
           </View>
-          <View style={{ width: '40%', height: '100%', borderWidth: windowWidth * 0.01, borderColor: 'white', borderRadius: windowWidth * 0.05, alignItems: 'center', justifyContent: 'center' }}>
-            <Image source={require("../Assets/google.png")} style={{ height: windowWidth * 0.1, width: windowWidth * 0.1 }} />
+          <View
+            style={{
+              width: '40%',
+              height: '100%',
+              borderWidth: windowWidth * 0.01,
+              borderColor: 'white',
+              borderRadius: windowWidth * 0.05,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image
+              source={require('../Assets/google.png')}
+              style={{height: windowWidth * 0.1, width: windowWidth * 0.1}}
+            />
           </View>
-        </View>
-        <View style={{ height: windowHeight * 0.05, width: '95%', marginTop: windowHeight * 0.05 }}>
-          <Text style={{ fontSize: windowWidth * 0.04, color: 'grey' }}>Don't have an account?</Text>
+        </View> */}
+        <View
+          style={{
+            height: windowHeight * 0.05,
+            width: '95%',
+            marginTop: windowHeight * 0.05,
+          }}>
+          <Text style={{fontSize: windowWidth * 0.04, color: 'grey'}}>
+            Don't have an account?
+          </Text>
           <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-            <Text style={{ fontSize: windowWidth * 0.055, fontWeight: 'bold', color: 'white' }}>Create account</Text>
+            <Text
+              style={{
+                fontSize: windowWidth * 0.035,
+                color: 'white',
+              }}>
+              Sign Up
+            </Text>
           </TouchableOpacity>
         </View>
       </Animatable.View>
--- Auth_page/SignPage.js
+++ Auth_page/SignPage.js
@@ -1,52 +1,205 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, Text, ImageBackground, TouchableOpacity, Image, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import { TextInput, ScrollView } from 'react-native-gesture-handler';
+import {TextInput, ScrollView} from 'react-native-gesture-handler';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-export default function SignPage({ navigation }) {
+export default function SignPage({navigation}) {
   return (
-    <ImageBackground style={{ flex: 1 }} source={require("../Assets/pxfuel2.jpg")}>
-      <Animatable.View useNativeDriver={true} animation="fadeIn" style={{ flex: 1, marginTop: windowHeight * 0.15, marginLeft: windowWidth * 0.05 }}>
-        <Text style={{ fontSize: windowWidth * 0.08, fontWeight: '800', color: 'white' }}>Create Account</Text>
-        <Text style={{ marginTop: windowHeight * 0.01, color: 'grey', fontSize: windowWidth * 0.035 }}>Please enter valid information to access your account</Text>
-        <ScrollView style={{ height: windowHeight * 0.6, width: '95%', marginTop: windowHeight * 0.06 }}>
-          <View style={{ flex: 1 }}>
-            <View style={{ flex: 1, width: '100%', backgroundColor: 'grey', marginBottom: windowHeight * 0.015, borderRadius: windowWidth * 0.04, opacity: 0.7, flexDirection: 'row', alignItems: 'center', paddingLeft: windowWidth * 0.04,height: windowWidth * 0.18 }}>
-              <Image source={require("../Assets/phone-call.png")} style={{ height: windowWidth * 0.05, width: windowWidth * 0.05 }} />
-              <TextInput style={{ flex: 1, marginLeft: windowWidth * 0.025 }} placeholder='Enter Phone' placeholderTextColor={'white'}></TextInput>
+    <ImageBackground
+      style={{flex: 1}}
+      source={require('../Assets/pxfuel2.jpg')}>
+      <Animatable.View
+        useNativeDriver={true}
+        animation="fadeIn"
+        style={{
+          flex: 1,
+          marginTop: windowHeight * 0.15,
+          marginLeft: windowWidth * 0.05,
+        }}>
+        <Text
+          style={{
+            fontSize: windowWidth * 0.08,
+            fontWeight: '800',
+            color: 'white',
+          }}>
+          Create Account
+        </Text>
+        <Text
+          style={{
+            marginTop: windowHeight * 0.01,
+            color: 'grey',
+            fontSize: windowWidth * 0.035,
+          }}>
+          Please enter valid information to access your account
+        </Text>
+        <ScrollView
+          style={{
+            height: windowHeight * 0.6,
+            width: '95%',
+            marginTop: windowHeight * 0.06,
+          }}>
+          <View style={{flex: 1}}>
+            <View
+              style={{
+                flex: 1,
+                width: '100%',
+                backgroundColor: 'grey',
+                marginBottom: windowHeight * 0.015,
+                borderRadius: windowWidth * 0.04,
+                opacity: 0.7,
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingLeft: windowWidth * 0.04,
+                height: windowWidth * 0.18,
+              }}>
+              <Image
+                source={require('../Assets/phone-call.png')}
+                style={{height: windowWidth * 0.05, width: windowWidth * 0.05}}
+              />
+              <TextInput
+                style={{flex: 1, marginLeft: windowWidth * 0.025}}
+                placeholder="Enter Phone"
+                placeholderTextColor={'white'}
+              />
             </View>
-            <View style={{ flex: 1, width: '100%', backgroundColor: 'grey', marginBottom: windowHeight * 0.015, borderRadius: windowWidth * 0.04, opacity: 0.7, flexDirection: 'row', alignItems: 'center', paddingLeft: windowWidth * 0.04,height: windowWidth * 0.18 }}>
-              <Image source={require("../Assets/user.png")} style={{ height: windowWidth * 0.05, width: windowWidth * 0.05 }} />
-              <TextInput style={{ flex: 1, marginLeft: windowWidth * 0.025 }} placeholder='Full Name' placeholderTextColor={'white'}></TextInput>
+            <View
+              style={{
+                flex: 1,
+                width: '100%',
+                backgroundColor: 'grey',
+                marginBottom: windowHeight * 0.015,
+                borderRadius: windowWidth * 0.04,
+                opacity: 0.7,
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingLeft: windowWidth * 0.04,
+                height: windowWidth * 0.18,
+              }}>
+              <Image
+                source={require('../Assets/user.png')}
+                style={{height: windowWidth * 0.05, width: windowWidth * 0.05}}
+              />
+              <TextInput
+                style={{flex: 1, marginLeft: windowWidth * 0.025}}
+                placeholder="Full Name"
+                placeholderTextColor={'white'}
+              />
             </View>
-            <View style={{ flex: 1, width: '100%', backgroundColor: 'grey', marginBottom: windowHeight * 0.015, borderRadius: windowWidth * 0.04, opacity: 0.7, flexDirection: 'row', alignItems: 'center', paddingLeft: windowWidth * 0.04,height: windowWidth * 0.18 }}>
-              <Image source={require("../Assets/at.png")} style={{ height: windowWidth * 0.05, width: windowWidth * 0.05 }} />
-              <TextInput style={{ flex: 1, marginLeft: windowWidth * 0.025 }} placeholder='Email' placeholderTextColor={'white'}></TextInput>
+            <View
+              style={{
+                flex: 1,
+                width: '100%',
+                backgroundColor: 'grey',
+                marginBottom: windowHeight * 0.015,
+                borderRadius: windowWidth * 0.04,
+                opacity: 0.7,
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingLeft: windowWidth * 0.04,
+                height: windowWidth * 0.18,
+              }}>
+              <Image
+                source={require('../Assets/at.png')}
+                style={{height: windowWidth * 0.05, width: windowWidth * 0.05}}
+              />
+              <TextInput
+                style={{flex: 1, marginLeft: windowWidth * 0.025}}
+                placeholder="Email"
+                placeholderTextColor={'white'}
+              />
             </View>
-            <View style={{ flex: 1, width: '100%', backgroundColor: 'grey', marginBottom: windowHeight * 0.015, borderRadius: windowWidth * 0.04, opacity: 0.7, flexDirection: 'row', alignItems: 'center', paddingLeft: windowWidth * 0.04,height: windowWidth * 0.18 }}>
-              <Image source={require("../Assets/key.png")} style={{ height: windowWidth * 0.05, width: windowWidth * 0.05 }} />
-              <TextInput style={{ flex: 1, marginLeft: windowWidth * 0.025 }} placeholder='Password' placeholderTextColor={'white'}></TextInput>
+            <View
+              style={{
+                flex: 1,
+                width: '100%',
+                backgroundColor: 'grey',
+                marginBottom: windowHeight * 0.015,
+                borderRadius: windowWidth * 0.04,
+                opacity: 0.7,
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingLeft: windowWidth * 0.04,
+                height: windowWidth * 0.18,
+              }}>
+              <Image
+                source={require('../Assets/key.png')}
+                style={{height: windowWidth * 0.05, width: windowWidth * 0.05}}
+              />
+              <TextInput
+                style={{flex: 1, marginLeft: windowWidth * 0.025}}
+                placeholder="Password"
+                placeholderTextColor={'white'}
+              />
             </View>
           </View>
-        
-        <TouchableOpacity onPress={() => navigation.navigate('Main')}>
-          <View style={{ marginTop: windowHeight * 0.05, height: windowHeight * 0.07, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', width: '100%' }}>
-            <View style={{ height: '100%', width: windowWidth * 0.35, borderRadius: windowWidth * 0.04, backgroundColor: '#d1b3ff' }}>
-              <View style={{ height: '100%', width: '70%', backgroundColor: 'rgb(102, 0, 150)', borderRadius: windowWidth * 0.04, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ fontSize: windowWidth * 0.055, fontWeight: '800' }}>Create</Text>
+
+          <TouchableOpacity onPress={() => navigation.navigate('Main')}>
+            <View
+              style={{
+                marginTop: windowHeight * 0.05,
+                height: windowHeight * 0.07,
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                width: '100%',
+              }}>
+              <View
+                style={{
+                  height: windowHeight * 0.065,
+                  width: windowWidth * 0.35,
+                  borderRadius: windowWidth * 0.025,
+                  backgroundColor: 'rgb(104, 89, 235)',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{fontSize: windowWidth * 0.035, color: 'white'}}>
+                  Create
+                </Text>
+
+                {/* <View
+                  style={{
+                    height: '100%',
+                    width: '70%',
+                    backgroundColor: 'rgb(102, 0, 150)',
+                    borderRadius: windowWidth * 0.04,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+
+                </View> */}
               </View>
             </View>
-          </View>
-        </TouchableOpacity>
-        <View style={{ height: windowHeight * 0.08, width: '95%', marginTop: windowHeight * 0.05 }}>
-          <Text style={{ fontSize: windowWidth * 0.03, color: 'grey' }}>Already have an account?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Text style={{ fontSize: windowWidth * 0.055, fontWeight: '800', color: 'white' }}>Login</Text>
           </TouchableOpacity>
-        </View>
+          <View
+            style={{
+              height: windowHeight * 0.08,
+              width: '95%',
+              marginTop: windowHeight * 0.05,
+            }}>
+            <Text style={{fontSize: windowWidth * 0.03, color: 'grey'}}>
+              Already have an account?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text
+                style={{
+                  fontSize: windowWidth * 0.035,
+                  color: 'white',
+                }}>
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </Animatable.View>
     </ImageBackground>
